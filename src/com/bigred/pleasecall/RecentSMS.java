1d52759a44c7b0a9e7241dbb659b636e3b5c70a1
apokellypse <kly24@cornell.edu>
2014-02-09 04:35:50 -0500

    attempted to implement a class to track sms timestamps in a useful fashion

---
+++ src/com/bigred/pleasecall/RecentSMS.java
@@ -0,0 +1,33 @@
+package com.bigred.pleasecall;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import android.content.ContentResolver;
+import android.database.Cursor;
+import android.net.Uri;
+
+
+//gets the time of the last SMS message received (from a specific person)
+public class RecentSMS {
+	ContentResolver contentResolver = getContentResolver();
+	Cursor cursor = contentResolver.query( Uri.parse( "content://sms/inbox" ), null, null, null, null);
+	boolean moved = cursor.moveToFirst(); //moved stores whether or not the cursor was moved successfully
+	String date =  cursor.getString(cursor.getColumnIndex("date"));
+	long timestamp = Long.parseLong(date);
+	
+	//Calendar calendar = Calendar.getInstance();
+	//calendar.setTimeInMillis(timestamp);
+	Date recentDate = new Date(timestamp);
+	String recentDateStr = recentDate.toString();
+	
+	Calendar rightNow = Calendar.getInstance();
+	Date todaysDate = rightNow.getTime();
+	String todaysDateStr = todaysDate.toString();
+	
+	
+	private ContentResolver getContentResolver()  {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
